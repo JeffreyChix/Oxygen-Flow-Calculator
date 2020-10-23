649d3c9f99e29a9fffa5a9a4bcf2c316bf37e55c
JeffreyChix <chiedoziejeffrey@gmail.com>
2020-10-23 06:16:41 +0100

    Updates

---
+++ precache-manifest.2c36bbfff825a68cfcebecc278c93111.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91794698a56b4f304c61f5e1b26d38c1",
+    "url": "/Oxygen-Flow-Calculator/index.html"
+  },
+  {
+    "revision": "8f31c024cbd8a0021b84",
+    "url": "/Oxygen-Flow-Calculator/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "fe5ed80839d5220f3a93",
+    "url": "/Oxygen-Flow-Calculator/static/css/main.f842f298.chunk.css"
+  },
+  {
+    "revision": "8f31c024cbd8a0021b84",
+    "url": "/Oxygen-Flow-Calculator/static/js/2.f54349ea.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Oxygen-Flow-Calculator/static/js/2.f54349ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5ed80839d5220f3a93",
+    "url": "/Oxygen-Flow-Calculator/static/js/main.1c1dd33e.chunk.js"
+  },
+  {
+    "revision": "6f01fa7e30b65f9eb0be",
+    "url": "/Oxygen-Flow-Calculator/static/js/runtime-main.bbd169c7.js"
+  }
+]);
